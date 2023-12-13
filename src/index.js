d84ac0eb8b2b114ec2c1ca7ed30e45717bf278c7
Tanmayi Kasthuri <tanmayi.kasthuri@gmail.com>
2023-12-13 21:30:27 +0530

    deployment changes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes,Route} from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';//for deployment
 
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools//for deployment
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
